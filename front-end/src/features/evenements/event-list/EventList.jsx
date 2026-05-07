78a8990b17477b8430aacfc16345249d42baeeb5
zakaria <dr-robot2001@gmail.com>
2026-05-07 23:25:14 +0100

    Restore EventList from specific commit 4aaa9667b946cba5b3eb0b68b5ccb35abea73ca5

--- front-end/src/features/evenements/event-list/EventList.jsx
+++ front-end/src/features/evenements/event-list/EventList.jsx
@@ -8,49 +8,59 @@ import Pagination from "../../../components/common/Pagination";
 import EventCardSkeleton from "../../../components/common/EventCardSkeleton";
 import { useEffect } from "react";
 import { fetchEvents, setFilter, setPage } from "./eventsSlice";
+import { useTranslation } from "react-i18next";
+import i18n from "../../../i18n";
+
 
 function EventList() {
   const dispatch = useDispatch();
+  const { t } = useTranslation("events");
+  const { events, domains } = useSelector((state) => state);
 
   const {
-    data,
     loading,
     currentPage,
     totalPages,
     currentFilter,
     itemsPerPage,
-  } = useSelector((state) => state.events);
+  } = events;
 
-  const filters = ["Tout", "Eau", "Agricultur", "Aid", "Ramadan"];
+  const filters = domains.data.map( domain => ({ key:domain.id , label:domain.label }) )
+  const currentLang = i18n.language || 'fr';
 
   useEffect(() => {
-    dispatch(fetchEvents({ page: currentPage, filter: currentFilter }));
-  }, [dispatch, currentPage, currentFilter]);
+    dispatch(
+      fetchEvents({
+        page: currentPage,
+        filter: currentFilter,
+        lang: currentLang,
+      }),
+    );
+  }, [dispatch, currentPage, currentFilter, currentLang]);
 
   const renderEvents = () => {
-
     if (loading) {
       return Array.from({ length: itemsPerPage }, (_, i) => (
         <EventCardSkeleton key={`skeleton-${i}`} />
       ));
     }
 
-    return data.map((event) => <EventCard key={event.id} {...event} />);
+    return events.data.map((event) => <EventCard key={event.id} {...event} />);
   };
 
   const handleFilterChange = (filter) => {
-    dispatch(setFilter(filter))
+    dispatch(setFilter(filter));
   };
   const handlePageChange = (page) => {
-    dispatch(setPage(page))
+    dispatch(setPage(page));
   };
   return (
     <div className={styles.eventListPage}>
-      <PageHero title="Ne manquez rien de nos nouveautés" heroImg={heroImg} />
+      <PageHero title={t("events.hero.title")} heroImg={heroImg} />
 
       <div className={styles.eventListContainer}>
         <div className={styles.filtersContainer}>
-          <h2>Au cœur de l’action</h2>
+          <h2>{t("events.section_title")}</h2>
           <FilterButtons
             filters={filters}
             currentFilter={currentFilter}
